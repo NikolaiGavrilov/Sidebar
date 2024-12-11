21d599e5cd09d1eb6e5fe3ff954ac9e8a0aca246
NikolaiGavrilov <nikolaisibir@mail.ru>
2024-12-12 02:36:52 +0300

    Slightly improved layout and introduced animations

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -21,11 +21,19 @@ const bottomRoutes = [
 
 const Sidebar = (props) => {
   const { color } = props;
-  const [isOpened, setIsOpened] = useState(false);
-  const containerClassnames = classnames("sidebar", { opened: isOpened });
+  const [isOpened, setIsOpened] = useState(true);
+  const [activeRoute, setActiveRoute] = useState(null);
+  const containerClassnames = classnames("sidebar", {
+    opened: isOpened,
+  });
 
   const goToRoute = (path) => {
     console.log(`going to "${path}"`);
+  };
+
+  const handleRouteClick = (path) => {
+    setActiveRoute(path);
+    goToRoute(path);
   };
 
   const toggleSidebar = () => {
@@ -41,36 +49,36 @@ const Sidebar = (props) => {
       </div>
       <div className="logo">
         <img className="logo__img" src={logo} alt="TensorFlow logo" />
-        {isOpened && <span className="logo__title">TensorFlow</span>}
+        <span className="logo__title">TensorFlow</span>
         <div className="sidebar-toggler" onClick={toggleSidebar}>
-          <FontAwesomeIcon icon={isOpened ? "angle-left" : "angle-right"} />
+          <FontAwesomeIcon className="sidebar-toggler__icon" icon={isOpened ? "angle-left" : "angle-right"} />
         </div>
       </div>
       <div className="routes routes__upper">
         {routes.map((route) => (
           <div
-            className="route"
+            className={classnames("route route-upper", {
+              active: activeRoute === route.path,
+            })}
             key={route.title}
-            onClick={() => {
-              goToRoute(route.path);
-            }}
+            onClick={() => handleRouteClick(route.path)}
           >
             <FontAwesomeIcon className="route__icon" icon={route.icon} />
-            {isOpened && <span className="route__title">{route.title}</span>}
+            <span className="route__title">{route.title}</span>
           </div>
         ))}
       </div>
       <div className="routes routes__bottom">
         {bottomRoutes.map((route) => (
           <div
-            className="route"
+            className={classnames("route route-bottom", {
+              active: activeRoute === route.path,
+            })}
             key={route.title}
-            onClick={() => {
-              goToRoute(route.path);
-            }}
+            onClick={() => handleRouteClick(route.path)}
           >
             <FontAwesomeIcon className="route__icon" icon={route.icon} />
-            {isOpened && <span className="route__title">{route.title}</span>}
+            <span className="route__title">{route.title}</span>
           </div>
         ))}
       </div>
